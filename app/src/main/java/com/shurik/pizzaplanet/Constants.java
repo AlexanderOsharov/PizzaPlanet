7575ab0649668a76e91d1d38a50f560cb0c85fad
Melnik <mvldislav6745@gmail.com>
2023-04-11 18:01:15 +0300

    check

--- app/src/main/java/com/shurik/pizzaplanet/Constants.java
+++ app/src/main/java/com/shurik/pizzaplanet/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String FOURSQUARE_API = "fsq3ViJD4B1N5wGhUKpBqHxwyqZz3tzVZXsA50pdiNYsk60=";
     public static final String FOURSQUARE_ClientId = "D45ZVUWSLYQTLFJDZTNRGG1AAOLINVZBCNH5G3SJMVJHALRV";
     public static final String FOURSQUARE_ClientSecret = "M43FGOWAFWSYIMG44Y12LPYRYDCOFFBR0SHLWVEYDPGF1F3D";
+
+//    public static final String YANDEX
 }

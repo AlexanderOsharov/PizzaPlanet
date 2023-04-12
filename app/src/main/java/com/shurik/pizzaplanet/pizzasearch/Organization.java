69bc656d8f66bd1acf3b0d18afa1ed27020373e2
Melnik <mvldislav6745@gmail.com>
2023-04-12 11:57:09 +0300

    добавил классы Organization и OrganizationParser

---
+++ app/src/main/java/com/shurik/pizzaplanet/pizzasearch/Organization.java
@@ -0,0 +1,32 @@
+package com.shurik.pizzaplanet.pizzasearch;
+
+public class Organization {
+    private String address;
+    private String name;
+    private String id;
+
+    public Organization(String address, String name, String id) {
+        this.address = address;
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Addres: " + address + "\n" +
+                "Name: " + name + "\n" +
+                "ID: " + id;
+    }
+}

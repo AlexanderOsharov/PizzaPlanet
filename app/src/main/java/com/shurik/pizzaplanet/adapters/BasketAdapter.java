4a8368cf455bf32324bda26fc843d8b1b04d9da1
Александр Ошаров <sashosharov@gmail.com>
2023-04-14 06:36:02 +0300

    Добавил плохую заготовку для корзины

---
+++ app/src/main/java/com/shurik/pizzaplanet/adapters/BasketAdapter.java
@@ -0,0 +1,90 @@
+package com.shurik.pizzaplanet.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.shurik.pizzaplanet.R;
+import com.shurik.pizzaplanet.pizzasearch.Pizza;
+
+import java.util.List;
+
+public class BasketAdapter extends RecyclerView.Adapter<BasketAdapter.BasketViewHolder> {
+
+    private List<Pizza> pizzaList;
+
+    public BasketAdapter(List<Pizza> pizzaList) {
+        this.pizzaList = pizzaList;
+    }
+
+    @NonNull
+    @Override
+    public BasketViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.basket_item, parent, false);
+        return new BasketViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BasketViewHolder holder, int position) {
+        Pizza pizza = pizzaList.get(position);
+        holder.pizzaTitle.setText(pizza.getPizzaName());
+        holder.price.setText(pizza.getPizzaPrise());
+        Glide.with(holder.pizzaImage.getContext())
+                .load(pizza.getPizzaImageUrl())
+                .into(holder.pizzaImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return pizzaList.size();
+    }
+
+    class BasketViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView pizzaImage;
+        TextView pizzaTitle;
+        TextView price;
+        ImageView minus;
+        TextView quantity;
+        ImageView plus;
+
+        BasketViewHolder(View view) {
+            super(view);
+            pizzaImage = view.findViewById(R.id.pizza_image);
+            pizzaTitle = view.findViewById(R.id.pizza_title);
+            price = view.findViewById(R.id.price);
+            minus = view.findViewById(R.id.minus);
+            minus.setOnClickListener(this);
+            quantity = view.findViewById(R.id.quantity);
+            plus = view.findViewById(R.id.plus);
+            plus.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // get the pizza item at the clicked position
+            Pizza item = pizzaList.get(getAdapterPosition());
+
+            switch (v.getId()) {
+                case R.id.minus:
+                    int qty = Integer.parseInt(quantity.getText().toString());
+                    if (qty > 1) {
+                        qty--;
+                        quantity.setText(String.valueOf(qty));
+                    }
+                    break;
+
+                case R.id.plus:
+                    int qty1 = Integer.parseInt(quantity.getText().toString());
+                    qty1++;
+                    quantity.setText(String.valueOf(qty1));
+                    break;
+            }
+        }
+    }
+}
--- app/src/main/java/com/shurik/pizzaplanet/fragments/BasketFragment.java
+++ app/src/main/java/com/shurik/pizzaplanet/fragments/BasketFragment.java
@@ -2,65 +2,62 @@ package com.shurik.pizzaplanet.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.shurik.pizzaplanet.R;
+import com.shurik.pizzaplanet.adapters.BasketAdapter;
+import com.shurik.pizzaplanet.databinding.FragmentBasketBinding;
+import com.shurik.pizzaplanet.databinding.FragmentCustomerBinding;
+import com.shurik.pizzaplanet.pizzasearch.Organization;
+import com.shurik.pizzaplanet.pizzasearch.OrganizationAdapter;
+import com.shurik.pizzaplanet.pizzasearch.Pizza;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BasketFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class BasketFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentBasketBinding binding;
+    private RecyclerView recyclerView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public BasketFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BasketFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BasketFragment newInstance(String param1, String param2) {
-        BasketFragment fragment = new BasketFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private BasketAdapter basketAdapter;
+    LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_basket, container, false);
+        binding = FragmentBasketBinding.inflate(inflater, container, false);
+        recyclerView = binding.pizzaRecyclerview;
+        layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        List<Pizza> pizzaList = new ArrayList<>();
+        pizzaList.add(new Pizza("Маргаритта", "сыр с помидорчиками", "https://e0.edimdoma.ru/data/posts/0002/1429/21429-ed4_wide.jpg?1631194036","360"));
+        pizzaList.add(new Pizza("Боржоми", "сыр с огурчиками", "https://www.edimdoma.ru/system/images/contents/0001/3568/wide/85419-original.jpg?1628278420","800"));
+        pizzaList.add(new Pizza("1", "0000 u 1111", "https://avatars.mds.yandex.net/get-altay/2887807/2a000001744e74154b4e0274fa3024dacdf3/XXL","500"));
+        pizzaList.add(new Pizza("2", "0110", "https://chef.ru/wp-content/uploads/iz-abhazii-s-lyubovyu-uv-3_tilda5808565.jpg","200"));
+
+        updateUIWithPizzaVenues(pizzaList);
+    }
+
+    private void updateUIWithPizzaVenues(List<Pizza> pizzaVenuesList) {
+        basketAdapter = new BasketAdapter(pizzaVenuesList);
+        recyclerView.setAdapter(basketAdapter);
     }
 }
